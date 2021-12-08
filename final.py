7b02308ed0a4a3b0d9c42794326ff7c6e0125e9a
youngcho <syh225@gmail.com>
2021-12-08 16:40:06 +0900

    v2

--- final.py
+++ final.py
@@ -26,9 +26,6 @@ GPIO.output(TRIG, False)
 print("Waiting for sensor to settle")
 time.sleep(2)
 
-
-
-
 @app.route("/")
 def home():
     return render_template('final.html')
@@ -36,10 +33,6 @@ def home():
 @app.route("/dis")    
 def dis():                                  
     try:
-        	
-    
-
-
         GPIO.output(TRIG, True)   # Triger 핀에  펄스신호를 만들기 위해 1 출력
         time.sleep(0.00001)       # 10µs 딜레이 
         GPIO.output(TRIG, False)
@@ -51,7 +44,7 @@ def dis():
             
         check_time = stop - start
         distance = check_time * 34300 / 2
-        print("Distance : %.1f cm" % distance) 
+
         return "%.14f cm" % distance
 
         
